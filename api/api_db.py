232ab3f35093f93ff362bd84692e7e519a80cc5f
McKynlee Westman <mckynlee.westman@gmail.com>
2021-03-31 07:46:49 -0500

    No module named psycopg2 error

--- api/api_db.py
+++ api/api_db.py
@@ -1,6 +1,7 @@
 # Import the flask library to make code available to entire app
 import flask
 from flask import request, jsonify
+import psycopg2
 
 # Create Flask app object
 app = flask.Flask(__name__)
@@ -8,24 +9,50 @@ app = flask.Flask(__name__)
 # Start the debugger
 app.config["DEBUG"] = True 
 
-#Test data for catalog, each object in the list is called a dictionary
-books = [
-    {'id': 0,
-     'title': 'A Fire Upon the Deep',
-     'author': 'Vernor Vinge',
-     'first_sentence': 'The coldsleep itself was dreamless.',
-     'year_published': '1992'},
-    {'id': 1,
-     'title': 'The Ones Who Walk Away From Omelas',
-     'author': 'Ursula K. Le Guin',
-     'first_sentence': 'With a clamor of bells that set the swallows soaring, the Festival of Summer came to the city Omelas, bright-towered by the sea.',
-     'published': '1973'},
-    {'id': 2,
-     'title': 'Dhalgren',
-     'author': 'Samuel R. Delany',
-     'first_sentence': 'to wound the autumnal city.',
-     'published': '1975'}
-]
+
+# Connect to PG db
+# conn = psycopg2.connect("dbname=books_db user=postgres password=postgres")
+conn = psycopg2.connect(
+  host="localhost",
+  port=5432,
+  database="books_db",
+  # user="postgres",
+  # password="postgres"
+)
+
+def connect():
+  conn = None
+  try:
+    params = config()
+    print('Connecting to the PostgreSQL database...')
+    conn = psycopg2.connect(**params)
+
+    cur = conn.cursor()
+    print('PostgreSQL database version:')
+    cur.execute('SELECT version()')
+
+    db_version = cur.fetchone()
+    print(db_version)
+
+    cur.close()
+
+  except (Exception, psycopg2.DatabaseError) as error:
+    print(error)
+
+  finally:
+    if conn is not None:
+      conn.close()
+      print('Database connection closed.')
+
+if __name__ == '__main__':
+  connect()
+
+# Define data that will come back from db (SQLite)
+# def dict_factory(cursor, row):
+#   d = {}
+#   for idx, col in enumerate(cursor.description):
+#     d[col[0]] = row[idx]
+#   return d
 
 
 # Set the api path and method to be received on that path
@@ -43,7 +70,10 @@ def api_all():
 # GET data by specific ID: (id is provided by ?id=0)
 @app.route('/api/v1/resources/books', methods=['GET'])
 def api_id():
-  #if ID is provided as part of URL, then assign it to a variable
+ 
+
+
+  # if ID is provided as part of URL, then assign it to a variable
   if 'id' in request.args:
       id = int(request.args['id'])
   else:  #Otherwise, display an error in the browser
